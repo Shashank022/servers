f1a790e91847609223a201712027b8856bb4cfaf
shashi <shashi@localhost.localdomain>
2020-04-07 11:10:54 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/layout/Navbar';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Switch>
           <Route exact path='/' component={ Dashboard }></Route>
           <Route path='/projects/:id' component={ ProjectDetails }></Route>
+          <Route path='/signin' component={ SignIn }></Route>
+          <Route path='/signup' component={ SignUp }></Route>
+          <Route path='/createproject' component={ CreateProject }></Route>
         </Switch>
       </div>
     </BrowserRouter>
